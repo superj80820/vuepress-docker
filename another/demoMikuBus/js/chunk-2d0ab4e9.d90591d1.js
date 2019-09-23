2c4b28d77bae6c46052aa4b62dc1f26e8c757991
superj80820 <superj80820@gmail.com>
2019-09-24 02:26:36 +0800

    Add another folder to demo another static website

---
+++ another/demoMikuBus/js/chunk-2d0ab4e9.d90591d1.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ab4e9"],{"155d":function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],u=n("2877"),c={},i=Object(u["a"])(c,a,s,!1,null,null,null);t["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0ab4e9.d90591d1.js.map
