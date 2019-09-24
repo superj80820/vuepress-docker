f16d95917b85eed709e41a11ba39113fdaa49a68
superj80820 <superj80820@gmail.com>
2019-09-24 19:35:01 +0800

    Fix fbChat error display

--- docs/.vuepress/components/fbChat.vue
+++ docs/.vuepress/components/fbChat.vue
@@ -1,11 +1,5 @@
 <template>
-    <div>
         <div id="fb-root"></div>
-        <div class="fb-customerchat"
-        attribution=setup_tool
-        :page_id="pageId">
-        </div>
-    </div>
 </template>
 
 <script>
@@ -17,19 +11,36 @@ export default {
     },
     mounted() {
         window.fbAsyncInit = function() {
-        FB.init({
-        xfbml            : true,
-        version          : 'v4.0'
-        });
-    };
-    (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/zh_TW/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+            FB.init({
+            xfbml            : true,
+            version          : 'v4.0'
+            });
+        };
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/zh_TW/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+        (function(scope) {
+            /*
+            Can't use FB demo code.
+            ```
+            <div class="fb-customerchat"
+            attribution=setup_tool
+            :page_id="pageId">
+            </div>
+            ```
+            Because using `:page_id="pageId"` will failure.
+            I think it is v-bind timing is wrong.
+            */
+            const elem = document.createElement('div')
+            elem.setAttribute('class', 'fb-customerchat')
+            elem.setAttribute('attribution', 'setup_tool')
+            elem.setAttribute('page_id', scope.pageId)
+            document.body.appendChild(elem)
+        })(this);
     }
 }
 </script>
-
