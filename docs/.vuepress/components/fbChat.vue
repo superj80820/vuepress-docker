a25ad8f7cc7aeeed7cbe63b377b853186ba76f13
superj80820 <superj80820@gmail.com>
2019-09-26 03:03:56 +0800

    Update fbChat.vue create function

--- docs/.vuepress/components/fbChat.vue
+++ docs/.vuepress/components/fbChat.vue
@@ -1,5 +1,5 @@
 <template>
-        <div id="fb-root"></div>
+    <div id="fb-root"></div>
 </template>
 
 <script>
@@ -9,13 +9,25 @@ export default {
             pageId: config.PAGE_ID
         }
     },
+    methods: {
+        createFbChat(scope) {
+            const elem = document.createElement('div')
+            elem.setAttribute('class', 'fb-customerchat')
+            elem.setAttribute('attribution', 'setup_tool')
+            elem.setAttribute('page_id', scope.pageId)
+            document.body.appendChild(elem)
+        }
+    },
     mounted() {
-        window.fbAsyncInit = function() {
-            FB.init({
-            xfbml            : true,
-            version          : 'v4.0'
-            });
-        };
+        (function(scope) {
+            window.fbAsyncInit = function() {
+                FB.init({
+                    xfbml            : true,
+                    version          : 'v4.0'
+                });
+                scope.createFbChat(scope);
+            };
+        })(this);
         (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
@@ -23,24 +35,6 @@ export default {
         js.src = 'https://connect.facebook.net/zh_TW/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
-        (function(scope) {
-            /*
-            Can't use FB demo code.
-            ```
-            <div class="fb-customerchat"
-            attribution=setup_tool
-            :page_id="pageId">
-            </div>
-            ```
-            Because using `:page_id="pageId"` will failure.
-            I think it is v-bind timing is wrong.
-            */
-            const elem = document.createElement('div')
-            elem.setAttribute('class', 'fb-customerchat')
-            elem.setAttribute('attribution', 'setup_tool')
-            elem.setAttribute('page_id', scope.pageId)
-            document.body.appendChild(elem)
-        })(this);
     }
 }
 </script>
