51540ec575834003d7edfdf4844fbd23dfe5309f
superj80820 <superj80820@gmail.com>
2019-10-28 06:47:24 +0800

    Add Ahfargo_bus_bot_frontend preview

---
+++ projects/Ahfargo_bus_bot_frontend/precache-manifest.3ab047760f7786332faf9ea48366f41e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1349c45e24be8184485",
+    "url": "/projects/Ahfargo_bus_bot_frontend/css/app.3a704bdc.css"
+  },
+  {
+    "revision": "c1349c45e24be8184485",
+    "url": "/projects/Ahfargo_bus_bot_frontend/js/app.6576d487.js"
+  },
+  {
+    "revision": "1227b904a596feed4db3",
+    "url": "/projects/Ahfargo_bus_bot_frontend/css/chunk-73a08d23.f1b946f8.css"
+  },
+  {
+    "revision": "1227b904a596feed4db3",
+    "url": "/projects/Ahfargo_bus_bot_frontend/js/chunk-73a08d23.b4d46e5e.js"
+  },
+  {
+    "revision": "bee1b14097719026f352",
+    "url": "/projects/Ahfargo_bus_bot_frontend/css/chunk-vendors.ffe4a649.css"
+  },
+  {
+    "revision": "bee1b14097719026f352",
+    "url": "/projects/Ahfargo_bus_bot_frontend/js/chunk-vendors.cd799892.js"
+  },
+  {
+    "revision": "dbc1a6e224553a96fd91a38a9ab37e73",
+    "url": "/projects/Ahfargo_bus_bot_frontend/index.html"
+  },
+  {
+    "revision": "92c3bd84bf8a68556329ee093a716a96",
+    "url": "/projects/Ahfargo_bus_bot_frontend/img/busPreview.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/projects/Ahfargo_bus_bot_frontend/robots.txt"
+  }
+];
