c78307a399e3840a749a45b5d23699cc1d185a86
superj80820 <superj80820@gmail.com>
2019-11-26 21:00:55 +0800

    Add line_QR_reader project

---
+++ projects/Taiwan-receipt-lottery/precache-manifest.097d23861c661a7d10fe7883fe712a50.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eafe02969d2ec3fe508",
+    "url": "/projects/Taiwan-receipt-lottery/css/chunk-vendors.26a95597.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/projects/Taiwan-receipt-lottery/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cd45e72da9a4c4d9afca3716e598998b",
+    "url": "/projects/Taiwan-receipt-lottery/index.html"
+  },
+  {
+    "revision": "903472863cc089a15217",
+    "url": "/projects/Taiwan-receipt-lottery/js/app.5d8d74a3.js"
+  },
+  {
+    "revision": "2eafe02969d2ec3fe508",
+    "url": "/projects/Taiwan-receipt-lottery/js/chunk-vendors.2857a6ea.js"
+  },
+  {
+    "revision": "3d039d53e375abb98478cdf83925ab90",
+    "url": "/projects/Taiwan-receipt-lottery/manifest.json"
+  },
+  {
+    "revision": "b9dc9965a8929c5913622263a25f51a4",
+    "url": "/projects/Taiwan-receipt-lottery/media/征服宇宙.b9dc9965.mp3"
+  },
+  {
+    "revision": "0a0e5547ec76137d40b4c7ab2e9ce8ef",
+    "url": "/projects/Taiwan-receipt-lottery/media/發大財.0a0e5547.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/projects/Taiwan-receipt-lottery/robots.txt"
+  },
+  {
+    "revision": "b9dc9965a8929c5913622263a25f51a4",
+    "url": "/projects/Taiwan-receipt-lottery/sounds/征服宇宙.mp3"
+  },
+  {
+    "revision": "0a0e5547ec76137d40b4c7ab2e9ce8ef",
+    "url": "/projects/Taiwan-receipt-lottery/sounds/發大財.mp3"
+  },
+  {
+    "revision": "b85e2cfeff3993a97371c16b4f155195",
+    "url": "/projects/Taiwan-receipt-lottery/winningNum.json"
+  }
+]);
---
+++ projects/line_QR_reader/README.md
@@ -0,0 +1,1 @@
+# line_QR_reader
